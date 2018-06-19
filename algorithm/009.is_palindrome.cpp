6280dc1ba8895346a78513c08c65736093b05b18
cloudy064 <cloudy064@gmail.com>
2018-06-19 15:35:05 +0800

    replace vector with array

--- algorithm/009.is_palindrome.cpp
+++ algorithm/009.is_palindrome.cpp
@@ -4,13 +4,13 @@ public:
         if (x < 0) return false;
         if (x < 10) return true;
 
-        std::vector<int> digits;
+        int digits[10];
+        int len = 0;
         while (x != 0) {
-            digits.push_back(x % 10);
+            digits[len++] = x % 10;
             x /= 10;
         }
 
-        size_t len = digits.size();
         for (size_t i = 0; i < len / 2; i++) {
             if (digits[i] != digits[len - i - 1]) return false;
         }

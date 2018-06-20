9d30fa1883e574fc93c2f091b74a27674e4071f4
cloudy064 <cloudy064@gmail.com>
2018-06-21 01:32:28 +0800

    add solution for 014

--- algorithm/014.longest_common_prefix.cpp
+++ algorithm/014.longest_common_prefix.cpp
@@ -13,37 +13,61 @@ using namespace std;
 
 class Solution {
 public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        if (strs.size() == 1) return strs[0];
 
+        string prefix;
+        int first_length = strs[0].length();
+        for (int i = 0; i < first_length; ++i) {
+            char ch = strs[0][i];
+            for (int j = 1; j < strs.size(); ++j) {
+                if (i >= strs[j].length()) return prefix;
+                if (ch != strs[j][i]) return prefix;
+            }
+
+            prefix.push_back(ch);
+        }
+
+        return prefix;
+    }
 };
 
-class Test012Solution : public ::testing::Test {
+class Test014Solution : public ::testing::Test {
 public:
     Solution sln;
 };
 
-TEST_F(Test012Solution, t1)
+TEST_F(Test014Solution, t1)
 {
-    EXPECT_EQ(sln.romanToInt("III"), 3);
+    string expect;
+    vector<string> strs;
+    string result = sln.longestCommonPrefix(strs);
+    EXPECT_TRUE(expect.compare(result) == 0);
 }
 
-TEST_F(Test012Solution, t2)
+TEST_F(Test014Solution, t2)
 {
-    EXPECT_EQ(sln.romanToInt("IV"), 4);
+    string expect = "prefix";
+    vector<string> strs = {"prefix"};
+    string result = sln.longestCommonPrefix(strs);
+    EXPECT_TRUE(expect.compare(result) == 0);
 }
 
-TEST_F(Test012Solution, t3)
+TEST_F(Test014Solution, t3)
 {
-    EXPECT_EQ(sln.romanToInt("IX"), 9);
+    string expect = "fl";
+    vector<string> strs = {"flower","flow","flight"};
+    string result = sln.longestCommonPrefix(strs);
+    EXPECT_TRUE(expect.compare(result) == 0);
 }
 
-TEST_F(Test012Solution, t4)
+TEST_F(Test014Solution, t4)
 {
-    EXPECT_EQ(sln.romanToInt("LVIII"), 58);
-}
-
-TEST_F(Test012Solution, t5)
-{
-    EXPECT_EQ(sln.romanToInt("MCMXCIV"), 1994);
+    string expect;
+    vector<string> strs = {"dog","racecar","car"};
+    string result = sln.longestCommonPrefix(strs);
+    EXPECT_TRUE(expect.compare(result) == 0);
 }
 
 GTEST_MAIN

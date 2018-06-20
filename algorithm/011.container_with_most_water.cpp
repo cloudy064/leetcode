bc374e074821eed5aa019a1733d8cef9effc60c3
cloudy064 <cloudy064@gmail.com>
2018-06-21 00:20:05 +0800

    add solution for 011

---
+++ algorithm/011.container_with_most_water.cpp
@@ -0,0 +1,65 @@
+//
+// Created by cloudy064 on 2018/6/21.
+//
+
+
+#include "common.h"
+
+using namespace std;
+
+/// Solution for https://leetcode-cn.com/problems/string-to-integer-atoi/description/
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int first = 0;
+        int second = static_cast<int>(height.size() - 1);
+        int max = 0;
+        do {
+            bool first_smaller = (height[first] < height[second]);
+            int area = (first_smaller ? height[first] : height[second]) * (second - first);
+            if (area > max) max = area;
+
+            first_smaller ? ++first : --second;
+        } while(first < second);
+
+        return max;
+    }
+};
+
+class Test011Solution : public ::testing::Test {
+public:
+    Solution sln;
+};
+
+TEST_F(Test011Solution, t1)
+{
+    vector<int> height = {1,1,1,1,1};
+    ASSERT_EQ(sln.maxArea(height), 4);
+}
+
+TEST_F(Test011Solution, t2)
+{
+    vector<int> height = {1,1,3,1,1};
+    ASSERT_EQ(sln.maxArea(height), 4);
+}
+
+TEST_F(Test011Solution, t3)
+{
+    vector<int> height = {3,1,3,1,1};
+    ASSERT_EQ(sln.maxArea(height), 6);
+}
+
+TEST_F(Test011Solution, t4)
+{
+    vector<int> height = {3,1};
+    ASSERT_EQ(sln.maxArea(height), 1);
+}
+
+TEST_F(Test011Solution, t5)
+{
+    vector<int> height = {2,3,4,5,18,17,6};
+    ASSERT_EQ(sln.maxArea(height), 17);
+}
+
+GTEST_MAIN

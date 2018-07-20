0e47057006057227d11eb187cdb95b36c2cbe994
cloudy064 <cloudy064@gmail.com>
2018-07-20 10:55:03 +0800

    add solution for 010.regular_expression_matching

--- algorithm/010.regular_expression_matching.cpp
+++ algorithm/010.regular_expression_matching.cpp
@@ -9,99 +9,53 @@ using namespace std;
 
 class Solution {
 public:
-    const int P_SINGLE = 0;
-    const int P_REPEAT = 1;
-
-    void preparse(const string& p, string& pre_chars, vector<int>& pre_pattern, vector<int>& anchor) {
-        char prev_c = '*';
-
-        int length = 0;
-        for (char c : p) {
-            assert(prev_c != '*' || c != '*'); // valid pattern for this problem
-
-            if (c == '*') {
-                assert(!pre_chars.empty());
-                assert(!pre_pattern.empty());
-                pre_pattern.back() = P_REPEAT;
-                if (length >= 2 && pre_pattern[length - 2] == P_REPEAT) {
-                    if (pre_chars[length - 2] == '.'
-                            || pre_chars[length - 2] == pre_chars[length - 1]) {
-                        pre_chars.pop_back();
-                        pre_pattern.pop_back();
-
-                        --length;
-                    } else if (pre_chars[length - 1] == '.') {
-                        pre_chars.pop_back();
-                        pre_pattern.pop_back();
-                        pre_chars.back() = '.';
-
-                        --length;
-                    }
-                }
-            } else {
-                pre_chars.push_back(c);
-                pre_pattern.push_back(P_SINGLE);
-                ++length;
-				if (length >= 2 && pre_pattern[length - 2] == P_REPEAT)
-				{
-					if (pre_chars[length - 2] == pre_chars[length - 1])
-					{
-						pre_pattern[length - 2] = P_SINGLE;
-						pre_pattern[length - 1] = P_REPEAT;
-					}
-				}
-            }
-
-            prev_c = c;
+    bool isMatchCore(string s, string p, int s_index, int p_index)
+    {
+        if (p_index >= p.length())
+        {
+            return s_index >= s.length();
         }
 
-		prev_c = pre_chars[0];
-		int i = -1;
-		while (++i < length && pre_chars[i] == '.' && pre_pattern[i] != P_REPEAT);
-		if (i >= length) return;
+        if (s_index >= s.length())
+        {
+            if ((p.length() - p_index) % 2 != 0) return false;
 
-		prev_c = pre_chars[i++];
-		for (; i < length; ++i)
-		{
-			if (pre_chars[i] == prev_c) continue;
-			if (pre_pattern[i] == P_REPEAT) continue;
+            for (int i = p_index; i < p.length(); i += 2)
+            {
+                if (i + 1 >= p.length()) return false;
 
-			prev_c = pre_chars[i];
-			anchor.push_back(i);
-		}
+                if (p[i + 1] != '*') return false;
+            }
+
+            return true;
+        }
+
+        if (p_index + 1 < p.length() && p[p_index + 1] == '*')
+        {
+            if (isMatchCore(s, p, s_index, p_index + 2)) return true;
+
+            if (p[p_index] == s[s_index] || p[p_index] == '.')
+            {
+                return isMatchCore(s, p, s_index + 1, p_index);
+            }
+
+            return false;
+        }
+        else
+        {
+            if (p[p_index] == s[s_index] || p[p_index] == '.')
+            {
+                return isMatchCore(s, p, s_index + 1, p_index + 1);
+            }
+
+            return false;
+        }
     }
 
     bool isMatch(string s, string p) {
         assert(p.compare("*") != 0);
 
-        string pre_chars;
-        vector<int> pre_pattern;
-		vector<int> anchor;
-        preparse(p, pre_chars, pre_pattern, anchor);
-		int s_idx = 0;
-		int p_idx = 0;
-		for (int i = 0; i < anchor.size(); ++i)
-		{
-			p_idx = anchor[i];
-			char anchor_ch = pre_chars[p_idx];
-			if (anchor_ch == '.')
-			{
-				++s_idx;
-				continue;
-			}
-
-			for (; s_idx < s.length(); ++s_idx)
-			{
-				if (pre_pattern[p_idx] == P_REPEAT) break;
-
-				if (s[s_idx] != pre_chars[p_idx])
-				{
-					if (p_idx == 0) return false;
-					if (pre_chars[p_idx - 1] != '.') return false;
-					continue;
-				}
-			}
-		}
+        return isMatchCore(s, p, 0, 0);
     }
 };
 
@@ -110,8 +64,36 @@ public:
     Solution sln;
 };
 
-TEST_F(Test010Solution, negative) {
+TEST_F(Test010Solution, t1) {
+    ASSERT_FALSE(sln.isMatch("aa", "a"));
+}
 
+TEST_F(Test010Solution, t2) {
+    ASSERT_TRUE(sln.isMatch("aa", "a*"));
+}
+
+TEST_F(Test010Solution, t3) {
+    ASSERT_TRUE(sln.isMatch("ab", ".*"));
+}
+
+TEST_F(Test010Solution, t4) {
+    ASSERT_TRUE(sln.isMatch("aab", "c*a*b"));
+}
+
+TEST_F(Test010Solution, t5) {
+    ASSERT_FALSE(sln.isMatch("acaabbaccbbacaabbbb", "a*.*b*.*a*aa*a*"));
+}
+
+TEST_F(Test010Solution, t6) {
+    ASSERT_TRUE(sln.isMatch("", "c*c*"));
+}
+
+TEST_F(Test010Solution, t7) {
+    ASSERT_TRUE(sln.isMatch("", ".*"));
+}
+
+TEST_F(Test010Solution, t8) {
+    ASSERT_TRUE(sln.isMatch("", ""));
 }
 
 GTEST_MAIN

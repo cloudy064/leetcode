59d11197b092d61cf9400f1cb6bc85c7af7b5871
cloudy064 <cloudy064@gmail.com>
2019-02-22 16:48:13 +0800

    commit solution for 058

---
+++ algorithm/058.length-of-last-word.cpp
@@ -0,0 +1,50 @@
+//
+// Created by ³£ÓÀÔÅ on 2019/2/22.
+//
+
+#include "common.h"
+
+using namespace std;
+
+/// Solution for https://leetcode-cn.com/problems/length-of-last-word/
+
+class Solution {
+public:
+	int lengthOfLastWord(string s) {
+		int length = s.length();
+		if (length == 0) return 0;
+
+		int result = 0;
+		int i = length - 1;
+		while (i >= 0 && s[i--] == ' ');
+
+		++i;
+		while (i >= 0 && s[i--] != ' ') ++result;
+
+		return result;
+	}
+};
+
+class Test058Solution : public ::testing::Test {
+public:
+	Solution sln;
+};
+
+TEST_F(Test058Solution, t1) {
+	EXPECT_EQ(sln.lengthOfLastWord(""), 0);
+	EXPECT_EQ(sln.lengthOfLastWord(" "), 0);
+}
+
+TEST_F(Test058Solution, t2) {
+	EXPECT_EQ(sln.lengthOfLastWord("abc"), 3);
+	EXPECT_EQ(sln.lengthOfLastWord(" abc"), 3);
+	EXPECT_EQ(sln.lengthOfLastWord("abc   "), 3);
+}
+
+TEST_F(Test058Solution, t3) {
+	EXPECT_EQ(sln.lengthOfLastWord("hello world"), 5);
+	EXPECT_EQ(sln.lengthOfLastWord(" hello world"), 5);
+	EXPECT_EQ(sln.lengthOfLastWord("hello world   "), 5);
+}
+
+GTEST_MAIN
